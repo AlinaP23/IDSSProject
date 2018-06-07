b962ae62de73c782f36627e30c228c406112ca10
Alina Pollklaesener <alina.pollklaesener@sap.com>
2018-06-07 18:25:42 +0200

    fixed UI - added attributes

--- IDSSProject/Controllers/HomeController.cs
+++ IDSSProject/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.IO;
 using System.Web;
 using System.Web.Mvc;
 using IDSSProject.Models;
+using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace IDSSProject.Controllers
 {
@@ -39,11 +41,10 @@ namespace IDSSProject.Controllers
         [HttpPost]
         public ActionResult Result(Customer customer)
         {
-            //Mock - Implement Connection to Machine Learning Part here
             string filePath = @"C:\Users\D060248\Desktop\IDSS\PW3\IDSSProject\IDSSProject\csv-output\file.csv";
             string delimiter = ";";
 
-            string header = "age; job; marital; education; default; balance; housing; loan; contact; day; month; duration; campaign; pdays; previous; poutcome";
+            string header = "age; job; marital; education; default; balance; housing; loan; contact; day; month; duration; campaign; pdays; previous; poutcome; emp.var.rate; cons.price.idx; cons.conf.idx; euribor3m; nr.employed;";
             string clientData = customer.Age.ToString() + delimiter
                               + customer.Job + delimiter
                               + customer.MaritalStatus + delimiter
@@ -59,12 +60,20 @@ namespace IDSSProject.Controllers
                               + customer.NumberOfContactsThis + delimiter
                               + customer.DaysSinceLastContact.ToString() + delimiter
                               + customer.NumberOfContactsPrior.ToString() + delimiter
-                              + customer.OutcomePreviousCampaign + delimiter;
+                              + customer.OutcomePreviousCampaign + delimiter
+                              + customer.EmploymentVariationRate + delimiter
+                              + customer.ConsumerPriceIndex + delimiter
+                              + customer.Euribor3Month + delimiter
+                              + customer.NumberOfEmployees + delimiter;
+
             StringBuilder sb = new StringBuilder();
             sb.AppendLine(header);
             sb.AppendLine(clientData);
             System.IO.File.WriteAllText(filePath, sb.ToString());
 
+            //string htmlResponse = Post("localhost:8000/getPrediction/", "csv=" + sb.ToString());
+            
+            //Mock - Implement Connection to Machine Learning Part here
             Outcome outcome = new Outcome();
             outcome.Success = true;
 
@@ -87,7 +96,44 @@ namespace IDSSProject.Controllers
             outcome.FifthInfluenceType = "Contact Type:";
             outcome.FifthInfluenceValue = "Mobile";
             outcome.FifthImageLink = "/Images/Influences/Mobile.png";
+
             return View(outcome);
+        }
+
+        public static string Post(string url, object postData)
+        {
+            HttpWebRequest httpWReq = (HttpWebRequest)WebRequest.Create(url);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, postData);
+
+            byte[] data = ms.ToArray();
+
+            httpWReq.Method = "POST";
+            httpWReq.ContentType = "application/x-www-form-urlencoded";
+            httpWReq.ContentLength = data.Length;
+
+            using (Stream newStream = httpWReq.GetRequestStream())
+            {
+                newStream.Write(data, 0, data.Length);
+            }
+
+            HttpWebResponse response = (HttpWebResponse)httpWReq.GetResponse();
+
+            Stream stream = response.GetResponseStream();
+
+            Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+
+            StreamReader streamReader = new StreamReader(stream, encode);
+
+            string html = streamReader.ReadToEnd();
+
+            response.Close();
+
+            streamReader.Close();
+
+            return html;
         }
     }
 }
--- IDSSProject/Models/Customer.cs
+++ IDSSProject/Models/Customer.cs
@@ -7,6 +7,7 @@ namespace IDSSProject.Models
 {
     public class Customer
     {
+        // bank client data 
         public int Age { get; set; }
         public string Job { get; set; }
         public string MaritalStatus { get; set; }
@@ -15,13 +16,22 @@ namespace IDSSProject.Models
         public string HousingLoan { get; set; }
         public string PersonalLoan { get; set; }
         public string Credit { get; set; }
+        // related with the last contact of the current campaign
         public string ContactType { get; set; }
         public int LastContactDay { get; set; }
         public string LastContactMonth { get; set; }
         public int Duration { get; set; }
+        // other attributes
         public int NumberOfContactsThis { get; set; }
         public int NumberOfContactsPrior { get; set; }
         public int DaysSinceLastContact { get; set; }
         public string OutcomePreviousCampaign { get; set; }
+        // social and economic context attributes
+        public int EmploymentVariationRate { get; set; }
+        public int ConsumerPriceIndex { get; set; }
+        public int ConsumerConfidenceIndex { get; set; }
+        public int Euribor3Month { get; set; }
+        public int NumberOfEmployees { get; set; }
+
     }
 }
